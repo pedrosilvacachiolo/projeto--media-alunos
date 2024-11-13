2efe9df39a8afed957da332ba091abd980f3fd9c
pedrosilvacachiolo <pedrohenriquecachiolo4@gmail.com>
2024-11-13 12:27:50 -0300

    Add funcionalidades de verificação de aprovação

--- media.py
+++ media.py
@@ -9,6 +9,12 @@ def calcular_media():
     # Calcula a média aritmética
     media = (nota1 + nota2 + nota3) / 3
     
+    def caucular_media_ponderada(nota1, nota2, nota3)
+    #cauculando a media ponderada
+    peso1, peso2, peso3 = 1, 1, 2
+    media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3)
+    return media 
+    
     # Exibe a média
     print(f"A média do aluno é: {media:.2f}")
     
